92969b07fc450dab8dd1ec92bb041116ea877354
sthasaroj13 <sarojshrestha651@gmail.com>
2023-12-21 12:48:53 +0545

    object

--- object2.js
+++ object2.js
@@ -1,3 +1,4 @@
+// you can add object into object in js 
  const user ={
     email : "sarojshrestha651@gmail.com",
     username:{
@@ -9,3 +10,34 @@
 
     } 
  console.log(user.username.fullname);
+ //object assign  this   static method helps  to comibne various objects
+ const obj1 = {1:"a",2:"b"}
+ const obj2 ={3:"c",4:"d"}
+ const obj3 =Object.assign({},obj1,obj2) //{} this is target and obj1 and obj2 is sources
+ const obj4 = {...obj1,...obj2} // this is spread  syntex  which allow you to iterate the values
+
+ console.log(obj3); 
+ console.log(obj4); 
+ console.log(Object.keys(user)) // this is the object call of any object and it output come in arrary which can apply in loop 
+ console.log(Object.values(user))
+ console.log (user.hasOwnProperty('saroj'))
+
+
+ // object de-structure 
+  const  carvalue ={
+    car :"Nexion",
+    brand :"TATA",
+    price :"22,50,500"
+  }
+ const {brand}= carvalue  //using {} in the object after object is made is called de-structure
+ console.log(brand)
+  
+
+ //json
+ // this is the format of jason
+ {
+
+
+    "name":"saroj stha"
+    "address": "kathmandu"
+ }
